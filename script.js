ec20b153d5383d17b945a714c9c9eddbf29a485c
anjali313477 <anjalitadiyal5@outlook.com>
2024-08-28 10:07:32 +0530

    add validation

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ btn.addEventListener("click", (event) => {
     event.preventDefault();
     let weight = document.querySelector('#weight').value;
     let height = document.querySelector('#height').value;
-     
-    height = height / 100;
-    let BMI = (weight / Math.pow(height, 2));
-    document.querySelector('#result').value = BMI;
 
-    if (height == "" || weight == "") {
-        alert("All fields are manadetory to fill")
+    if (height == "" && weight == "") {
+        swal("All Fields are mandatory to fill!", "", "warning");
+        return false
+    }else if(height.toString().length>=4 || weight.toString().length>=4){
+        swal("Please Enter appropriate value", "", "warning");
         return false
     }
-    else if(BMI < 18) {
-        document.querySelector("#classification").value = "Underweight"
-    } else if (BMI>18 && BMI<=24){
-        document.querySelector("#classification").value =" Normalweight"
-    }else if(BMI>25 && BMI<=29){
-        document.querySelector("#classification").value =" Overweight"
-    }else if(BMI>30){
-        document.querySelector("#classification").value = "Obese"
+
+    height = height / 100;
+    let BMI = (weight / Math.pow(height, 2));
+    document.querySelector('#result').innerHTML = `Your BMI = ${parseInt(BMI)}`;
+
+    if (BMI < 18) {
+        document.querySelector("#classification").innerHTML = "UNDERWEIGHT"
+    } else if (BMI > 18 && BMI <= 24) {
+        document.querySelector("#classification").innerHTML = " NORMALWEIGHT"
+    } else if (BMI > 25 && BMI <= 29) {
+        document.querySelector("#classification").innerHTML = " OVERWEIGHT"
+    } else if (BMI > 30) {
+        document.querySelector("#classification").innerHTML = "OBESE"
     }
 })
